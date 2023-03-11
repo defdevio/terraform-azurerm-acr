b656f65c167210a9cd1ea1771610940d4cfaac15
Tony Owens <anthony.owens1987@gmail.com>
2023-03-11 09:19:18 -0800

    Removed extra copy of gitignore. Added resource id outuput. Added DS_Store to ignore file

--- output.tf
+++ output.tf
@@ -1,5 +1,4 @@
-output "admin_kube_config" {
-  value       = try(azurerm_kubernetes_cluster.aks[0].kube_admin_config_raw, null)
-  sensitive   = true
-  description = "The admin kubeconfig for the AKS cluster."
+output "resource_ids" {
+  value       = azurerm_container_registry.acr[*].id
+  description = "The resource ids for the Azure Container Registries."
 }
